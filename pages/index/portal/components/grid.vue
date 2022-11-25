fd2675baf1e9881157d573c6b88653935794437f
JamesCurtis <jamescurtis@foxmail.com>
2022-11-25 12:16:04 +0800

    fix: 首页取消swiper，取消虚拟滚动，实现共用一根滚动条

--- pages/index/portal/components/grid.vue
+++ pages/index/portal/components/grid.vue
@@ -1,19 +1,19 @@
 <template>
 	<u-grid :col='4' class='' :border='false'>
 		<u-grid-item class='grid-nav'>
-			<image class="grid-nav--img" src="@/static/logo.png" mode="aspectFill"></image>
+			<image class="grid-nav__img" src="@/static/logo.png" mode="aspectFill"></image>
 			<text>图书分类</text>
 		</u-grid-item>
 		<u-grid-item class='grid-nav'>
-			<image class="grid-nav--img" src="@/static/logo.png" mode="aspectFill"></image>
+			<image class="grid-nav__img" src="@/static/logo.png" mode="aspectFill"></image>
 			<text>合作洽谈</text>
 		</u-grid-item>
 		<u-grid-item class='grid-nav'>
-			<image class="grid-nav--img" src="@/static/logo.png" mode="aspectFill"></image>
+			<image class="grid-nav__img" src="@/static/logo.png" mode="aspectFill"></image>
 			<text>邀请有奖</text>
 		</u-grid-item>
 		<u-grid-item class='grid-nav'>
-			<image class="grid-nav--img" src="@/static/logo.png" mode="aspectFill"></image>
+			<image class="grid-nav__img" src="@/static/logo.png" mode="aspectFill"></image>
 			<text>常见问题</text>
 		</u-grid-item>
 	</u-grid>
@@ -42,34 +42,8 @@
 </script>
 
 <style lang="scss" scoped>
-	.content-wrapper {
-		.block {
-			margin: 0 20rpx;
-			box-sizing: border-box;
-		}
-
-		.search-bar-block {
-			display: flex;
-			gap: 20rpx;
-			align-items: center;
-			margin-bottom: 20rpx;
-
-			.search-bar {
-				flex: 1;
-			}
-
-			.category {
-				height: 100%;
-			}
-		}
-
-		/deep/ .Swiper-mfw-index-box {
-			background-color: transparent;
-		}
-	}
-
 	.grid-nav {
-		.grid-nav--img {
+		&__img {
 			width: 100rpx;
 			height: 100rpx;
 			margin-bottom: 10rpx;
---
+++ pages/index/portal/components/waterfall.vue
@@ -0,0 +1,35 @@
+<template>
+	<u-waterfall v-model="list">
+		<template v-slot:left="{leftList}">
+			<view v-for="(item, index) in leftList" :key="index">
+				<good-list-item :good='item'></good-list-item>
+				<!-- 这里编写您的内容，item为您传递给v-model的数组元素 -->
+			</view>
+		</template>
+		<template v-slot:right="{rightList}">
+			<view v-for="(item, index) in rightList" :key="index">
+				<good-list-item :good='item'></good-list-item>
+				<!-- 这里编写您的内容，item为您传递给v-model的数组元素 -->
+			</view>
+		</template>
+	</u-waterfall>
+</template>
+
+<script>
+	export default {
+		name: 'PortalWaterfall',
+		data() {
+			return {}
+		},
+		props: {
+			list: {
+				type: Array,
+				required: true
+			}
+		},
+		methods: {}
+	}
+</script>
+
+<style>
+</style>
